79860256ab9ca0b8a8504cc296b74025be4bd21d
perinbaraja <102756719+Perinbaraja@users.noreply.github.com>
2022-07-06 14:09:20 +0530

    url update

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const config = {
   MONGO_URL:
     "mongodb://rainmongo:OSSawJsHkv85Hv6GA9uAKbEKtEX0bMlnyJ8W8HOEcHHudLsf0CAsjTtOdxY9zZfhuHT3UTMExsDMnCsOpJQG4Q==@rainmongo.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@rainmongo@",
   JWT_SECRET: "dev",
-  PORT: 5100,
+  PORT: 8080,
   MAIL_SERVICE: "gmail",
   SENDER_MAIL: "rpmongotest@gmail.com",
   MAIL_PASSWORD: "qpomthjiesygydjn",
