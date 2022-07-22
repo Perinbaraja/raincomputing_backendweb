41066f3246cdf288c6e4c6f964fb2e6ad9ee3ddb
perinbaraja <102756719+Perinbaraja@users.noreply.github.com>
2022-07-22 16:51:40 +0530

    config.js update

--- config.js
+++ config.js
@@ -1,13 +1,13 @@
 const config = {
   MONGO_URL:
-    "mongodb://localhost:27017/Rain",
+    "mongodb://rainmongo:OSSawJsHkv85Hv6GA9uAKbEKtEX0bMlnyJ8W8HOEcHHudLsf0CAsjTtOdxY9zZfhuHT3UTMExsDMnCsOpJQG4Q==@rainmongo.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@rainmongo@",
   JWT_SECRET: "dev",
   PORT: 8080,
   MAIL_SERVICE: "gmail",
   SENDER_MAIL: "rpmongotest@gmail.com",
   MAIL_PASSWORD: "qpomthjiesygydjn",
   RESET_PASSWORD: "dev123",
-  FE_URL:"http://localhost:3000"
+  FE_URL: "http://localhost:3000",
 };
 
 module.exports = config;
