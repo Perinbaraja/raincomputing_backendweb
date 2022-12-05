e77664d9ec36345493789cd2ac4ea8bc32cbfc7a
perinbaraja <102756719+Perinbaraja@users.noreply.github.com>
2022-12-05 12:18:35 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 const config = {
-  MONGO_URL: "mongodb://localhost:27017/Rain",
+  MONGO_URL:
+    "mongodb://raincomputingcosmosdb:tBYDpH68hIKiWL1dd72FlUV7m8tn3rqy6OV0fVWDSuzvSJ8XtovbzRP6bG4xMPKIwfTCHHr2AIveACDbx3ff6w%3D%3D@raincomputingcosmosdb.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@raincomputingcosmosdb@",
   JWT_SECRET: "dev",
   PORT: 8080,
   MAIL_SERVICE: "gmail",
