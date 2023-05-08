d4b1b6d8e43514c3b7a569f4e5929c0c1c5b5198
gokulgk2000 <110904612+gokulgk2000@users.noreply.github.com>
2023-05-08 23:21:01 +0530

    pin msg and complete case api

--- routes/messageRoute.js
+++ routes/messageRoute.js
@@ -14,6 +14,7 @@ router.post("/mailChat", messageController.MAIL_CHAT);
 router.post("/getsendernameById", messageController.GETSENDERBYNAMEID);
 router.post("/getgroupnameById", messageController.GETGROUPBYNAMEID);
 router.post("/pinnedmsgById", messageController.PINNEDMESSAGE);
+router.post("/unpinnedmsgById", messageController.UNPINNEDMESSAGE);
 router.post("/notes", messageController.FILENOTES);
 router.post("/getPinnedMsg", messageController.GETPINMESSAGES);
 router.post("/updateMsg", messageController.UPDATE_MESSAGE);
