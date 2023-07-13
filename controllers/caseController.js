fd466cf233020ff6450efe713e62795c55d913c8
gokulgk2000 <gokulkrishnan0149@gmail.com>
2023-07-13 11:48:15 +0530

    collapse subcase update

--- controllers/caseController.js
+++ controllers/caseController.js
@@ -6,7 +6,7 @@ const axios =require("axios")
 
 const CREATE = async (req, res) => {
   try {
-    const { caseId, caseName, members, admin,serialNumber,docDate,docEvent } = req.body;
+    const { caseId, caseName, members, admin,serialNumber,docDate,docEvent,maincaseId ,isSubcase} = req.body;
     const isCaseId = await Case.findOne({ caseId });
     if (isCaseId) return res.json({ msg: "Case Id Already existing" });
     const struturedMembers = members.map((m) => ({ id: m, addedBy: admin }));
@@ -17,6 +17,8 @@ const CREATE = async (req, res) => {
       notifyMembers: members,
       admins: [admin],
       serialNumber,
+      maincaseId:maincaseId,
+      isSubcase: isSubcase
     };
     const createdCase = await Case.create(caseQuery);
     if (createdCase) {
@@ -58,6 +60,7 @@ const GETBYUSERID = async (req, res) => {
           },
         },
         aflag: true,
+        isSubcase: { $ne : true}
       },
       null,
       { limit: limit, skip: skip }
@@ -118,31 +121,32 @@ const UPDATE_CASE = async (req, res) => {
     return res.json({ msg: err || config.DEFAULT_RES_ERROR });
   }
 };
-const EVENT_CREATE = async (req, res) => {
-  const { caseId, events } = req.body;
-  try {
-    const cases = await Case.findById(caseId);
-    if (!cases) {
-      return res.status(404).json({ error: "Case not found" });
+  const EVENT_CREATE = async (req, res) => {
+    const { caseId, events} = req.body;
+    try {
+      const cases = await Case.findById(caseId);
+      if (!cases) {
+        return res.status(404).json({ error: "Case not found" });
+      }
+      const newEvent = events.map((event) => ({
+        eventId:event.eventId,
+        docEvent: event?.docEvent,
+        eventText: event?.eventText.map((textObj) => ({
+          text: textObj.text,
+          docDate: textObj.docDate,
+        })),
+        receivedDate: event?.receivedDate,
+      }));
+      console.log("newEvent:", newEvent);
+      cases.events.push(...newEvent);
+      await cases.save();
+      res.status(200).json({ success: true, data: cases });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: "Server error" });
     }
-    const newEvent = events.map((event) => ({
-      docDate: event?.docDate,
-      docEvent: event?.docEvent,
-      eventText: event?.eventText.map((textObj) => ({
-        text: textObj.text,
-        docDate: textObj.docDate,
-      })),
-      receivedDate: event?.receivedDate,
-    }));
-    console.log("newEvent:", newEvent);
-    cases.events.push(...newEvent);
-    await cases.save();
-    res.status(200).json({ success: true, data: cases });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: "Server error" });
-  }
-};
+  };
+
 // const EVENT_CREATE = async (req, res) => {
 //   const { caseId, events } = req.body;
 //   try {
@@ -316,6 +320,122 @@ const SEARCHCASEBYSNO= async (req, res ) =>{
     })
   }
 }
+const CREATE_SUBCASE = async (req, res) => {
+  const { caseId, subCase } = req.body;
+
+  try {
+    // Find the parent case based on the provided caseId
+    const parentCase = await Case.findById(caseId);
+
+    if (!parentCase) {
+      return res.status(404).json({ error: "Parent case not found" });
+    }
+
+    // Create subcases using the provided subCase array
+    const createdSubCases = [];
+    for (const subCaseItem of subCase) {
+      const { caseName, serialNumber, caseMembers, notifyMembers, admins, aflag, isCompleted } = subCaseItem;
+
+      // Create a new subcase object
+      const newSubCase = {
+        caseName,
+        serialNumber,
+        caseMembers,
+        notifyMembers,
+        admins,
+        aflag,
+        isCompleted
+      };
+
+      // Push the new subcase to the parent case's subCase array
+      const createdSubCase = await parentCase.subCase.create(newSubCase);
+      createdSubCases.push(createdSubCase);
+
+      // Add the created subcase to the parent case
+      parentCase.subCase.push(createdSubCase);
+    }
+
+    // Save the parent case to persist the changes
+    await parentCase.save();
+
+    // Get the _id of each created subcase
+    const createdSubCaseIds = createdSubCases.map((subCase) => subCase._id);
+
+    // Create a group query
+    const groupQuery = {
+      caseId: parentCase._id,
+      // groupMembers: createdSubCaseIds.map((subCase) => ({ id: subCase, addedBy: admin })),
+      isGroup: true,
+      // admins: [admin],
+    };
+
+    // Create the group
+    const createdGroup = await Group.create(groupQuery);
+
+    // Return the created subcases, their IDs, and the group ID
+    return res.json({
+      success: true,
+      SubCase: createdSubCases,
+      CreatedSubCaseIds: createdSubCaseIds,
+      GroupId: createdGroup._id,
+    });
+  } catch (error) {
+    // Handle any errors that occur during the process
+    console.error(error);
+    res.status(500).json({ error: "Failed to create subcases" });
+  }
+}
+const CASEIDBY_SUBCASES = async (req, res) => {
+  const { caseId } = req.body;
+  try {
+    const caseIdSubCases = await Case.find({
+      maincaseId: caseId,
+      isSubcase:  true,
+      caseMembers: {
+        $elemMatch: {
+          isActive: true,
+        },
+      },
+      aflag: true,
+    })
+      .populate([
+        { path: "caseMembers.id", select: "firstname lastname profilePic email" },
+        { path: "caseMembers.addedBy", select: "firstname lastname" },
+      ])
+      .exec();
+
+    if (caseIdSubCases && caseIdSubCases.length > 0) {
+      return res.json({
+        success: true,
+        caseIdSubCases,
+      });
+    } else {
+      return res.json({ msg: "No subcases found" });
+    }
+  } catch (error) {
+    // Handle any errors that occur during the process
+    console.error(error);
+    res.status(500).json({ error: "Failed to retrieve subcases" });
+  }
+};
+const GET_ALL_SUBCASES = async (req,res) => {
+
+  try {
+  const allsubCases = await Case.find({isSubcase : true})
+  if(allsubCases ) {
+    return res.json({
+      success : true,
+      allsubCases
+    })
+  }
+  }catch (error) {
+    // Handle any errors that occur during the process
+    console.error(error);
+    res.status(500).json({ error: "Failed to retrieve subcases" });
+  }
+}
+
+
 
 
 module.exports.caseController = {
@@ -329,5 +449,8 @@ module.exports.caseController = {
   ADD_ADMIN,
   GETCOMPLETEDCASES,
   REMOVE_ADMIN,
-  SEARCHCASEBYSNO
+  SEARCHCASEBYSNO,
+  CREATE_SUBCASE,
+  CASEIDBY_SUBCASES,
+  GET_ALL_SUBCASES
 };
